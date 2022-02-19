eda0870fc6473fe4a695a0b9324cff22f2ce2979
nurislam.askaruly <nurislam.askaruly@qdata.kz>
2022-02-19 11:26:38 +0600

    asd

--- amigo/admin_analytics_service/admin.py
+++ amigo/admin_analytics_service/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Location)
+
+
+admin.site.register(models.Delivery_Company)
+
+
+admin.site.register(models.Order)
+
+admin.site.register(models.Offer)
+
+admin.site.register(models.UserProfile)
